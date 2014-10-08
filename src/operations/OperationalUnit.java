1d9d7f38a526809e357681f815514450b495d696
Nathan O <nro5@pitt.edu>
2014-10-08 00:39:45 -0400

    More changes

--- src/operations/OperationalUnit.java
+++ src/operations/OperationalUnit.java
@@ -1,29 +1,38 @@
 package operations;
 
+import reservationStation.ReservationStation;
 import basicUnits.ClockDependentInterface;
 import basicUnits.ClockDependentUnit;
 import basicUnits.instruction.Instruction;
 
 public abstract class OperationalUnit extends ClockDependentUnit implements ClockDependentInterface
 {
-	public final int latency;
+	public final int minLatency;
 	public final boolean isPipelineable;
-	private final Instruction[] pipeline;
+	protected final Instruction[] pipeline;
 	private boolean outputIsReady;
 	private boolean outputIsTaken;
 	private Number output;
 	
-	public OperationalUnit(int latency, boolean isPipelineable)
+	private final ReservationStation reservationStation;
+	
+	public OperationalUnit(int minLatency, boolean isPipelineable, ReservationStation reservationStation)
 	{
 		super(Integer.MAX_VALUE);
-		this.latency = latency;
+		this.minLatency = minLatency;
 		this.isPipelineable = isPipelineable;
-		this.pipeline = new Instruction[this.latency];
+		this.pipeline = new Instruction[this.minLatency+1];
+		this.reservationStation = reservationStation;
 	}
 	
 	public boolean isOutputReady()
 	{
 		return this.outputIsReady;
+	}
+	
+	public Instruction getOutputInstruction()
+	{
+		return this.pipeline[this.pipeline.length-1];
 	}
 	
 	public Number getOutput()
@@ -36,13 +45,58 @@ public abstract class OperationalUnit extends ClockDependentUnit implements Cloc
 		return output;
 	}
 	
+	public boolean isReadyForInput()
+	{
+		if(this.isPipelineable)
+		{
+			return this.pipeline[0] == null;
+		}
+		
+		//checking non-pipelineable units
+		for(int i = 0; i < this.pipeline.length; i++)
+		{
+			if(this.pipeline[i] != null)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean prepInstructionForPipeline()
+	{
+		if(!this.isReadyForInput())
+		{
+			return false;
+		}
+		
+		this.pipeline[0] = this.reservationStation.getInstruction();
+		return this.pipeline[0] != null;
+	}
+	
 	@Override
 	public void newClockCycle()
 	{
 		super.newClockCycle();
-		this.outputIsReady = false;
-		this.outputIsTaken = false;
-		this.output = null;
+		
+		//if output is taken, then discard it and allow another to take its place
+		if(this.outputIsTaken)
+		{
+			this.outputIsReady = false;
+			this.outputIsTaken = false;
+			this.output = null;
+			this.pipeline[this.pipeline.length-1] = null;
+		}
+		
+		//move pipeline
+		for(int i = this.pipeline.length-2; i >= 0; i--)
+		{
+			if(this.pipeline[i+1] == null)
+			{
+				this.pipeline[i+1] = this.pipeline[i];
+				this.pipeline[i] = null;
+			}
+		}
 	}
 	
 	/**
@@ -51,7 +105,7 @@ public abstract class OperationalUnit extends ClockDependentUnit implements Cloc
 	@Override
 	public boolean doOneOperation()
 	{
-		if(super.doOneOperation())
+		if(this.pipeline[this.pipeline.length-1] != null && super.doOneOperation())
 		{
 			this.output = this.operate();
 			return true;
@@ -59,5 +113,5 @@ public abstract class OperationalUnit extends ClockDependentUnit implements Cloc
 		return false;
 	}
 	
-	public abstract Number operate();
+	protected abstract Number operate();
 }
--- src/reservationStation/ReservationStation.java
+++ src/reservationStation/ReservationStation.java
@@ -1,14 +1,68 @@
 package reservationStation;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import basicUnits.ClockDependentInterface;
 import basicUnits.ClockDependentUnit;
+import basicUnits.instruction.Instruction;
 
 public class ReservationStation extends ClockDependentUnit implements ClockDependentInterface
 {
+	private final int maxInstructionsInQueue;
+	private final Queue<Instruction> instructionQueue;
+	private final Queue<Instruction> inPipeline;
 
-	public ReservationStation(int numOperationsPerCycle)
+	public ReservationStation(int maxInstructionsInQueue)
 	{
-		super(numOperationsPerCycle);
+		super(1);
+		this.maxInstructionsInQueue = maxInstructionsInQueue;
+		this.instructionQueue = new LinkedList<Instruction>();
+		this.inPipeline = new LinkedList<Instruction>();
 	}
+	
+	public boolean addInstruction(Instruction i)
+	{
+		if(this.instructionQueue.size()+this.inPipeline.size() >= this.maxInstructionsInQueue)
+		{
+			return false;
+		}
+		this.instructionQueue.add(i);
+		return true;
+	}
+	
+	public Instruction getInstruction()
+	{
+		if(!super.doOneOperation())
+		{
+			return null;
+		}
 
+		Instruction ready = this.instructionQueue.poll();
+		if(ready == null)
+		{
+			return null;
+		}
+		this.inPipeline.add(ready);
+		return ready;
+	}
+	
+	public Instruction removeInstruction()
+	{
+		return this.inPipeline.poll();
+	}
+	
+	public void newClockCycle()
+	{
+		super.newClockCycle();
+	}
+	
+	public boolean doOneOperation()
+	{
+		if(!super.doOneOperation())
+		{
+			return false;
+		}
+		return true;
+	}
 }
